d89fad8872c9fcd3ea34e38d6af2cfed6fa62b27
Bloom <bloomtauta@gmail.com>
2023-07-17 13:12:43 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    console.log(e.target.new-task-description.value);
+    form.reset()
+  });
 });
+
+function buildToDo(toDo) {
+    const p = document.createElement('p')
+    const btn = document.createElement('button')
+    btn.addEventListener('click', deleteTodo)
+    btn.textContent = 'X'
+    p.appendChild(btn)
+    p.textContent = `${toDo} `
+    console.log(p)
+    document.querySelector("#tasks").appendChild(p)
+};
+
+function deleteTodo(e){
+    e.target.parentNode().remove()
+};
