02091cac2eaa8a79373ab211349c43f0bb17d3ba
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-07-10 14:42:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let form = document.querySelector('form')
+  let form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
-    console.log(e.target.new-task-description.value);
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value)
     form.reset()
   });
 });
@@ -10,14 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
 function buildToDo(toDo) {
     const p = document.createElement('p')
     const btn = document.createElement('button')
-    btn.addEventListener('click', deleteTodo)
-    btn.textContent = 'X'
-    p.appendChild(btn)
-    p.textContent = `${toDo} `
-    console.log(p)
+    btn.addEventListener('click', deleteTodo);
+    btn.textContent = 'X';
+    
+    p.textContent = `${toDo} `;
+    p.appendChild(btn);
     document.querySelector("#tasks").appendChild(p)
 };
 
 function deleteTodo(e){
-    e.target.parentNode().remove()
+    e.target.parentNode.remove()
 };
